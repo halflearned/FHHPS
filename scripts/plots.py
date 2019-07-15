3b2f57e0d26ed5764bd318ea6b30599c0dc24597
halflearned <halflearned@gmail.com>
2019-07-14 19:44:13 -0700

    adding conditional means

--- scripts/plots.py
+++ scripts/plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-filenames = glob("/Users/vitorh/Documents/FHHPS/scripts/results/*.csv.bz2")
+filenames = glob("/Users/vitorh/Documents/fhhps/scripts/script_out/results.csv")
 df = pd.concat([pd.read_csv(f) for f in filenames])
 
 df_grp = df.query(
--- scripts/short_script.py
+++ scripts/short_script.py
@@ -1,9 +1,19 @@
 import os
 import subprocess
+from collections import OrderedDict as ODict
+from fractions import Fraction
 from random import choice
 
 from fhhps.estimator import *
 from fhhps.utils import *
+
+
+def as_frac(x):
+    frac = Fraction(x).limit_denominator(10000)
+    if frac.denominator != 1:
+        return f"{frac.numerator}/{frac.denominator}"
+    else:
+        return f"{frac.numerator}"
 
 
 def on_sherlock():
@@ -29,18 +39,20 @@ def get_unique_filename(prefix="results", rnd=None, commit=True):
 
 
 filename = os.path.join("script_out", get_unique_filename())
-names = ["VarA", "VarB", "VarC", "CovAB", "CovAC", "CovBC"]
+mean_names = ["EA", "EB", "EC"]
+cov_names = ["VarA", "VarB", "VarC", "CovAB", "CovAC", "CovBC"]
 for s in range(1000):
 
     if on_sherlock():
         n = choice([1000, 5000, 20000])
+        output_bw1_const = choice([.1, .25, .5, 1, 2, 3, 5, 10])
+        shock_bw1_const = choice([.1, .25, .5, 1, 2, 3, 5, 10])
     else:
+        output_bw1_const = 1
+        shock_bw1_const = 1
         n = 2000
 
-    output_bw1_const = choice([.1, .5, 1, 3, 5, 10, 20])
     output_bw2_const = output_bw1_const
-
-    shock_bw1_const = choice([.1, .5, 1, 3, 5, 10, 20])
     shock_bw2_const = shock_bw1_const
 
     shock_bw1_alpha = 1 / 6
@@ -78,22 +90,31 @@ for s in range(1000):
     coef_cond_means = get_coef_cond_means(X, Z, output_cond_means, shock_means)
     coef_cond_cov = get_coef_cond_cov(X, Z, output_cond_cov, shock_cov)
 
-    valid = get_valid_cond_cov(X, Z, 1.)
+    mean_valid = get_valid_cond_means(X, Z, 1.)
+    cov_valid = get_valid_cond_cov(X, Z, 1.)
 
-    estimate = get_coef_cov(coef_cond_means, coef_cond_cov, valid)
+    mean_estimate = get_coef_means(coef_cond_means, mean_valid)
+    cov_estimate = get_coef_cov(coef_cond_means, coef_cond_cov, cov_valid)
     truth = get_true_coef_cov(fake)
 
-    res = pd.DataFrame(data=[{"n": n,
-                              "output_bw1_const": output_bw1_const,
-                              "output_bw2_const": output_bw2_const,
-                              "output_bw1_alpha": output_bw1_alpha,
-                              "output_bw2_alpha": output_bw2_alpha,
-                              "shock_bw1_const": shock_bw1_const,
-                              "shock_bw2_const": shock_bw2_const,
-                              "shock_bw1_alpha": shock_bw1_alpha,
-                              "shock_bw2_alpha": shock_bw2_alpha,
-                              "name": name,
-                              "value": est}
-                             for name, est in zip(names, estimate)])
-    res.to_csv(filename, header=s == 0, index=False, mode="a")
-    print(res)
+    config = ODict(**{"n": n,
+                      "output_bw1_const": as_frac(output_bw1_const),
+                      "output_bw2_const": as_frac(output_bw2_const),
+                      "output_bw1_alpha": as_frac(output_bw1_alpha),
+                      "output_bw2_alpha": as_frac(output_bw2_alpha),
+                      "shock_bw1_const": as_frac(shock_bw1_const),
+                      "shock_bw2_const": as_frac(shock_bw2_const),
+                      "shock_bw1_alpha": as_frac(shock_bw1_alpha),
+                      "shock_bw2_alpha": as_frac(shock_bw2_alpha)})
+
+    mean_res = pd.DataFrame(data=[ODict({**config, "name": name, "value": est})
+                                  for name, est in zip(mean_names, mean_estimate)])
+    cov_res = pd.DataFrame(data=[ODict({**config, "name": name, "value": est})
+                                 for name, est in zip(cov_names, cov_estimate)])
+
+    mean_res.to_csv(filename, header=s == 0, index=False, mode="a")
+    cov_res.to_csv(filename, header=s == 0, index=False, mode="a")
+
+    print(mean_res)
+    print(cov_res)
+    sdsd
